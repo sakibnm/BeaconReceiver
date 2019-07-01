44e0f1f1efbfa50e0f82a8b098d473188c13a503
Sakib <mmiazi@uncc.edu>
2019-07-01 18:02:39 -0400

    Works with EID UID and ALT and IBeacon

--- app/src/main/java/com/example/beaconreceiver/MainActivity.java
+++ app/src/main/java/com/example/beaconreceiver/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -18,23 +17,50 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
+
+import java.util.Collection;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BeaconConsumer, RangeNotifier {
 
+    private static final String TAG = "demo";
     BluetoothManager btManager;
     BluetoothAdapter btAdapter;
     BluetoothLeScanner btLeScanner;
+
+    BeaconManager beaconManager;
     Button buttonScan;
 
     //Permission flags...
     private final static int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        beaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
+        // Detect the main Eddystone-UID frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        // Detect the telemetry Eddystone-TLM frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT));
+        beaconManager.bind(this);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -48,11 +74,25 @@ public class MainActivity extends AppCompatActivity {
         btAdapter = btManager.getAdapter();
         btLeScanner = btAdapter.getBluetoothLeScanner();
 
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+// Detect the telemetry (TLM) frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
+// Detect the URL frame:
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT));
+        //Detect iBeacons...
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+
 
         //Bluetooth permission check...
         if (btAdapter != null && !btAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
         }
 
         // Make sure we have access coarse location enabled, if not, prompt the user to enable it
@@ -70,69 +110,52 @@ public class MainActivity extends AppCompatActivity {
             builder.show();
         }
 
-
-        buttonScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("scan", "Started scanning...");
-                startScanning();
-
-            }
-        });
-
     }
 
-
-
-    //Scanning....
-    private void startScanning(){
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                btLeScanner.startScan(bleScanner);
-            }
-        });
-    }
-
-    //Bluetooth scan callback...
-    private ScanCallback bleScanner = new ScanCallback() {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            Log.d("scan", "Device Name: " + result.getDevice().getName() + " rssi: " + result.getRssi());
-        }
-
-        @Override
-        public void onBatchScanResults(List<ScanResult> results) {
-            super.onBatchScanResults(results);
-            Log.d("scan", "onBatchScanResults: "+results.toString());
-        }
-
-        @Override
-        public void onScanFailed(int errorCode) {
-            super.onScanFailed(errorCode);
-            Log.d("scan", "onScanFailed: Scan Failed "+errorCode);
-        }
-    };
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                btLeScanner.stopScan(bleScanner);
-            }
-        });
+    public void onBeaconServiceConnect() {
+        Region region = new Region("all-beacons-region", null, null, null);
+        try {
+            beaconManager.startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        beaconManager.addRangeNotifier(this);
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                btLeScanner.stopScan(bleScanner);
+    public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+        for (Beacon beacon: collection) {
+            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
+                // This is a Eddystone-UID frame
+                Identifier namespaceId = beacon.getId1();
+                Identifier instanceId = beacon.getId2();
+                Log.d(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
+                        " and instance id: "+instanceId+
+                        " approximately "+beacon.getDistance()+" meters away."+", "+beacon.getExtraDataFields().toString());
+
+                // Do we have telemetry data?
+                if (beacon.getExtraDataFields().size() > 0) {
+                    long telemetryVersion = beacon.getExtraDataFields().get(0);
+                    long batteryMilliVolts = beacon.getExtraDataFields().get(1);
+                    long pduCount = beacon.getExtraDataFields().get(3);
+                    long uptime = beacon.getExtraDataFields().get(4);
+
+                    Log.d(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
+                            ", has been up for : "+uptime+" seconds"+
+                            ", has a battery level of "+batteryMilliVolts+" mV"+
+                            ", and has transmitted "+pduCount+" advertisements.");
+
+                }
+            }else if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x10) {
+                // This is a Eddystone-URL frame
+                String url = UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray());
+                Log.d(TAG, "I see a beacon transmitting a url: " + url +
+                        " approximately " + beacon.getDistance() + " meters away.");
+            }else{
+                Log.d(TAG, "didRangeBeaconsInRegion: "+beacon.getBluetoothAddress()+", "+beacon.getBluetoothName()+", "+beacon.getServiceUuid()+", "+beacon.getExtraDataFields().toString());
             }
-        });
+        }
     }
 }
